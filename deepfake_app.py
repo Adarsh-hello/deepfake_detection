6d1b5c49c9de4fa1badbbf9299a3b969049ba4ce
Adarsh Tiwari <adarsh200404@gmail.com>
2025-04-26 15:54:57 +0530

    Update deepfake_app.py

--- deepfake_app.py
+++ deepfake_app.py
@@ -6,7 +6,7 @@
       "provenance": [],
       "gpuType": "T4",
       "authorship_tag": "ABX9TyNRBHW11NOFGRNwiZ3SlB4M",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
